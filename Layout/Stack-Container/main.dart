eb7cce54d5fd9f5f84da5a9fc8b485f5b221060d
claulis <cla_ulis@hotmail.com>
2024-11-25 11:24:24 -0300

    ok

---
+++ Layout/Stack-Container/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Exemplo de Stack')),
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                width: 200,
+                height: 200,
+                color: Colors.blue,
+              ),
+              Container(
+                width: 150,
+                height: 150,
+                color: Colors.red,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.green,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
